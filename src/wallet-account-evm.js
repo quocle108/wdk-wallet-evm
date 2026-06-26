815d022772f398f9d067f66c58a972502ae1b480
claudiovb <claudiocarvalhovilasboas@gmail.com>
2026-06-26 16:28:36 -0300

    update JSDOC

--- src/wallet-account-evm.js
+++ src/wallet-account-evm.js
@@ -134,8 +134,8 @@ export default class WalletAccountEvm extends WalletAccountReadOnlyEvm {
   }
 
   /**
-   * Returns the account's address. If not set at construction time (e.g. lazy hardware signers),
-   * it asks the underlying signer to resolve it, then caches it locally.
+   * Returns the account's address. If it wasn't resolved at construction time (e.g hardware signers), it asks the
+   * underlying signer to resolve it, then caches it locally.
    *
    * @returns {Promise<string>} The account's address.
    */
